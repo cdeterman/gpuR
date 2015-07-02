da02e21e1e2129e6b3a44ac539cf7e776a6168e3
Charles Determan <cdetermanjr@gmail.com>
2015-07-02 09:56:40 -0500

    separated dynEigen methods so compiler happy in other child packages

--- inst/include/gpuR/dynEigen.hpp
+++ inst/include/gpuR/dynEigen.hpp
@@ -11,30 +11,38 @@ class dynEigen {
         int nr, nc;
         
     public:
-        dynEigen(SEXP A_)
-        {
-            A = Rcpp::as<Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>>(A_);
-            nr = A.rows();
-            nc = A.cols();
-        }
-        
-        dynEigen(Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> A_)
-        {
-            A = A_;
-            nr = A.rows();
-            nc = A.rows();
-        }
-        
-        dynEigen(int nr_in, int nc_in)
-        {
-            A = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>::Zero(nr_in,nc_in);
-            nr = nr_in;
-            nc = nc_in;
-        }
+        dynEigen(SEXP A_);
+        dynEigen(Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> A_);
+        dynEigen(int nr_in, int nc_in);
         
         T* ptr() { return &A(0); }
         int nrow() { return nr; }
         int ncol() { return nc; }
 };
+
+
+template<typename T>
+dynEigen<T>::dynEigen(SEXP A_)
+{
+    A = Rcpp::as<Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> >(A_);
+    nr = A.rows();
+    nc = A.cols();
+}
+
+template<typename T>
+dynEigen<T>::dynEigen(Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> A_)
+{
+    A = A_;
+    nr = A.rows();
+    nc = A.rows();
+}
+
+template<typename T>
+dynEigen<T>::dynEigen(int nr_in, int nc_in)
+{
+    A = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>::Zero(nr_in,nc_in);
+    nr = nr_in;
+    nc = nc_in;
+}
 
 #endif
