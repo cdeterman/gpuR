81b5a4b405c38a7fc185c7b1a53e9ae18508d351
Charles Determan <cdetermanjr@gmail.com>
2015-12-11 08:32:20 -0600

    additional docs and 'deepcopy' tests

---
+++ inst/include/gpuR/dynEigenVecSlice.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#ifndef DYNEIGEN_VEC_HPP
+#define DYNEIGEN_VEC_HPP
+
+#include <RcppEigen.h>
+
+template <class T> 
+class dynEigenVec {
+    private:
+        dynEigenVec() { } // private default constructor
+        Eigen::Matrix<T, Eigen::Dynamic, 1> A;
+        int size;
+        
+    public:
+        dynEigenVec(SEXP A_);
+        dynEigenVec(Eigen::Matrix<T, Eigen::Dynamic,1> A_);
+        dynEigenVec(int size_in);
+        dynEigenVec(Eigen::VectorBlock<Eigen::Map<Eigen::Matrix<T, Eigen::Dynamic, 1> >, Eigen::Dynamic>);
+        
+        T* ptr() { return &A(0); }
+        int length() { return size; }
+};
+
+#endif
