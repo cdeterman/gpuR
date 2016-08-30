ebe17c6f67a197da548911022f21aeb6b7041bbc
Charles Determan <cdetermanjr@gmail.com>
2016-08-30 13:02:49 -0500

    message to confirm if initContexts or packageStartupMessage is the problem

--- R/zzz.R
+++ R/zzz.R
@@ -10,8 +10,10 @@
         # initialize contexts
         # default_device <- initContexts()
         initContexts()
+        print("context initialization successful")
         # packageStartupMessage(paste0("gpuR ", packageVersion('gpuR'), "\nDefault device: ", default_device))
         packageStartupMessage(paste0("gpuR ", packageVersion('gpuR')))
+        print("startup message not problem")
     }
 }
 
