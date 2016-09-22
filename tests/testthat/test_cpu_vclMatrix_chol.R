19b08e80210a004c7ab1c345661a334adac7ec95
Charles Determan <cdetermanjr@gmail.com>
2016-09-22 15:34:22 -0500

    add cholesky tests for CPU

---
+++ tests/testthat/test_cpu_vclMatrix_chol.R
@@ -0,0 +1,56 @@
+library(gpuR)
+context("CPU vclMatrix chol decomposition")
+
+# set seed
+set.seed(123)
+
+ORDER <- 10
+
+# Base R objects
+X <- matrix(rnorm(ORDER^2), nrow=ORDER, ncol=ORDER)
+X <- X %*% t(X)
+# X <- matrix(c(5,1,1,3),2,2)
+# nsqA <- matrix(rnorm(20), nrow = 4)
+
+D <- chol(X)
+
+test_that("CPU vclMatrix Single Precision Matrix Cholesky Decomposition",
+          {
+              
+              has_cpu_skip()
+              
+              fgpuX <- vclMatrix(X, type="float")
+              # fgpuA <- vclMatrix(nsqA, type = "float")
+              
+              C <- chol(fgpuX)
+              
+              # print(C[])
+              
+              expect_is(C, "fvclMatrix")
+              expect_equal(C[], D, tolerance=1e-05, 
+                           info="float cholesky decomposition not equivalent")  
+              
+              
+              # expect_error(svd(fgpuA), "non-square matrix not currently supported for 'svd'",
+              #              info = "svd shouldn't accept non-square matrices")
+          })
+
+test_that("CPU vclMatrix Double Precision Matrix Cholesky Decomposition",
+          {
+              
+              has_cpu_skip()
+              
+              fgpuX <- vclMatrix(X, type="double")
+              
+              C <- chol(fgpuX)
+              
+              # print(C[])
+              
+              expect_is(C, "dvclMatrix")
+              expect_equal(C[], D, tolerance=.Machine$double.eps^0.5, 
+                           info="double cholesky decomposition not equivalent")  
+              
+              
+              # expect_error(svd(fgpuA), "non-square matrix not currently supported for 'svd'",
+              #              info = "svd shouldn't accept non-square matrices")
+          })
