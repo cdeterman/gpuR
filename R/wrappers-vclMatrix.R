abd343f47c48f5e47e90d30b461908c0af73278f
Determan <DetermanC@schneider.com>
2017-07-14 12:06:29 -0500

    forgot to commit vclMatrix wrapper too

--- R/wrappers-vclMatrix.R
+++ R/wrappers-vclMatrix.R
@@ -5,7 +5,7 @@ vclMatInitNumVec <- function(data, nrow, ncol, type, ctx_id){
     device <- if(is.null(ctx_id)) currentDevice() else listContexts()[ctx_id,]
     
     context_index <- ifelse(is.null(ctx_id), currentContext(), ctx_id)
-    device_index <- if(is.null(ctx_id)) as.integer(device$device_index) else device$device_index + 1L
+    device_index <- if(is.null(ctx_id)) as.integer(device$device_index) + 1L else device$device_index + 1L
 
     platform_index <- if(is.null(ctx_id)) currentPlatform()$platform_index else device$platform_index + 1L
     platform_name <- platformInfo(platform_index)$platformName
@@ -59,7 +59,7 @@ vclMatInitNumScalar <- function(data, nrow, ncol, type, ctx_id){
     device <- if(is.null(ctx_id)) currentDevice() else listContexts()[ctx_id,]
     
     context_index <- ifelse(is.null(ctx_id), currentContext(), ctx_id)
-    device_index <- if(is.null(ctx_id)) as.integer(device$device_index) else device$device_index + 1L
+    device_index <- if(is.null(ctx_id)) as.integer(device$device_index) + 1L else device$device_index + 1L
     
     platform_index <- if(is.null(ctx_id)) currentPlatform()$platform_index else device$platform_index + 1L
     platform_name <- platformInfo(platform_index)$platformName
@@ -119,7 +119,7 @@ vclMatInitIntVec <- function(data, nrow, ncol, type, ctx_id){
     device <- if(is.null(ctx_id)) currentDevice() else listContexts()[ctx_id,]
     
     context_index <- ifelse(is.null(ctx_id), currentContext(), ctx_id)
-    device_index <- if(is.null(ctx_id)) as.integer(device$device_index) else device$device_index + 1L
+    device_index <- if(is.null(ctx_id)) as.integer(device$device_index) + 1L else device$device_index + 1L
     
     platform_index <- if(is.null(ctx_id)) currentPlatform()$platform_index else device$platform_index + 1L
     platform_name <- platformInfo(platform_index)$platformName
@@ -183,7 +183,7 @@ vclMatInitIntScalar <- function(data, nrow, ncol, type, ctx_id){
     device <- if(is.null(ctx_id)) currentDevice() else listContexts()[ctx_id,]
     
     context_index <- ifelse(is.null(ctx_id), currentContext(), ctx_id)
-    device_index <- if(is.null(ctx_id)) as.integer(device$device_index) else device$device_index + 1L
+    device_index <- if(is.null(ctx_id)) as.integer(device$device_index) + 1L else device$device_index + 1L
     
     platform_index <- if(is.null(ctx_id)) currentPlatform()$platform_index else device$platform_index + 1L
     platform_name <- platformInfo(platform_index)$platformName
