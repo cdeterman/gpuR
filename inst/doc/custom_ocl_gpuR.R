77053aa868b8539ebd2277099d4ce52d9e1beb1f
Determan <DetermanC@schneider.com>
2017-10-19 08:38:29 -0500

    rebuild and cleanup vignettes

--- inst/doc/custom_ocl_gpuR.R
+++ inst/doc/custom_ocl_gpuR.R
@@ -4,6 +4,15 @@ opts_chunk$set(
 concordance=TRUE
 )
 
+## ----kernel_example, eval = FALSE----------------------------------------
+#  "__kernel void SAXPY(__global float* x, __global float* y, float a)
+#  {
+#      const int i = get_global_id(0);
+#  
+#      y [i] += a * x [i];
+#  }
+#  "
+
 ## ----cl_setup, eval=FALSE------------------------------------------------
 #  cl_args <- setup_opencl(objects = c("vclVector", "vclVector", "scalar"),
 #                          intents = c("IN", "OUT", "IN"),
